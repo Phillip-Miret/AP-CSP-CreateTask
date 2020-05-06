2841a8d4e2a23ce1145a14a370081287d0aa09b2
Phillip Miret <45930012+Phillip-Miret@users.noreply.github.com>
2020-05-06 14:47:38 -0400

    added number button

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ const ctx = canvas.getContext('2d');
 // canvas.height = window.innerHeight - 20;
 
 canvas.width = 1240;
-canvas.height = 740;
+canvas.height = 700;
 
-let c1 = new circle(canvas.width/2, canvas.height/2, 20, 0, 0);
+let numCircs = new Number();
+let circles = new Array[numCircs];
 let gravity = -0.5;
 
 
@@ -22,6 +23,13 @@ function circle(x, y , radius, dx, dy){
    this.dy = dy;
 }
 
+function getInput(){
+    let input = document.getElementById("numInput").value;
+   console.log(input);
+   let inputNum = input.match(/\d+/g);
+   inputNum = parseInt(inputNum[0]);
+   numCircs = inputNum;
+}
 
 
 function drawCircle(circle){
@@ -30,8 +38,10 @@ function drawCircle(circle){
     ctx.fill();
     if (circle.x + circle.radius >= canvas.width || circle.x - circle.radius <= 0)
         circle.dx *= -1;
-    if (circle.y + circle.radius >= canvas.height || circle.y - circle.radius <= 0){
+    if (circle.y + circle.radius >= canvas.height){ 
+        circle. y = canvas.height - circle.radius;
         circle.dy *= -1;
+        circle.dy += gravity;      
     } 
     circle.dy -= gravity;
     circle.x += circle.dx;
